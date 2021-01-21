7f93b8187b8094220942c8c5db1d67b8d11bec95
Michal <mjarmocik@gmail.com>
2021-01-21 14:59:12 +0100

    Migration to new database host. Changed regex to match new response.

--- functions/index.js
+++ functions/index.js
@@ -2,18 +2,22 @@ const functions = require('firebase-functions');
 const cors = require('cors')({
   origin: true,
 })
+
 const MongoClient = require('mongodb').MongoClient;
 const request = require('request-promise');
-const url = "mongodb://" + functions.config().database.username + ":" + functions.config().database.password + "@ds111765.mlab.com:11765/stores";
+
+const url = "mongodb+srv://" + functions.config().database.username + ":" + functions.config().database.password + "@cluster0.ra2cd.mongodb.net/stockfinder?retryWrites=true&w=majority"
+
 
 const stores_load = async function () {
   const client = new MongoClient(url, { useNewUrlParser: true });
   await client.connect();
-  const db = client.db('stores');
-  const collection = db.collection('all_stores');
+
+  const collection = client.db("stockfinder").collection("stores");
   let cursor = collection.find({});
   let stores = await cursor.toArray();
   client.close();
+
   return stores;
 }
 
@@ -22,7 +26,7 @@ const scrap_data = async function (stores, product_id) {
 
   var urls = new Array(stores.length);
 
-  var reg = /RESERVE_AND_COLLECT.*?distance.{3}(\d+\.?\d*)/;
+  var reg = /success.*?PAY_AND_COLLECT.*?0\smile\saway/;
 
   for (var i = 0; i < stores.length; i++) {
 
@@ -42,9 +46,12 @@ const scrap_data = async function (stores, product_id) {
   await Promise.all(promises).then((data) => {
     var results;
     for (var i = 0; i < stores.length; i++) {
-      if ((results = reg.exec(data[i])) !== null && results[1] < 2)
+      if (reg.test(data[i]))
         return_array.push({ 'Country': stores[i]['Country'], 'Town': stores[i]['Town'], 'Lon': stores[i]['Lon'], 'Lat': stores[i]['Lat'] });
     }
+  }).catch((e) => {
+    console.log(e);
+    throw 'Curry\'s server not responding'
   })
 
   return return_array;
@@ -59,14 +66,19 @@ exports.check = functions
       let product_code = /\d{8}/.exec(req.path);
 
       if (product_code == null) {
-        res.status(400).end();
+        res.status(400).end('Invalid input');
         return;
       }
 
+      try {
       let results = await scrap_data(stores, product_code[0]);
       res.status(200).send(results);
+      } catch (e) {
+        res.status(500).send(e);
+      }
     });
   });
+
 
 exports.coverage = functions
   .region('europe-west1')
